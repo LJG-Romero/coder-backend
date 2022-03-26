b8ede2d90cd1d53b9e38cb706ba02f7438cdd339
LJG-Romero <luucas.jg@gmail.com>
2022-03-26 03:50:00 -0300

    Ex 3.Challenge - First endpoint

--- ex2_fs.js
+++ ex2_fs.js
@@ -31,8 +31,9 @@ class Container {
     async getAll() {
         try{
             let data = await fs.promises.readFile(this.fileName, 'utf-8');
-            let dataStrg = JSON.stringify(data);
-            console.log(dataStrg);
+            let dataStrg = JSON.parse(data);
+            // console.log(dataStrg);
+            return dataStrg;
         }
         catch(err){
             console.log(`Ups, file missed ... Error: ${err}`);
@@ -40,13 +41,18 @@ class Container {
         
     }
     async deleteById(id){
-        let data = await fs.promises.readFile(this.fileName, 'utf-8');
-        let dataStrg = JSON.stringify(data);
-        for (const product of dataStrg) {
-            if (id === product.id){
-                let trash = dataStrg.splice(id,1);
-                console.log(`product eliminated`);
+        try{
+            let data = await fs.promises.readFile(this.fileName, 'utf-8');
+            let dataStrg = JSON.stringify(data);
+            for (const product of dataStrg) {
+                if (id === product.id){
+                    let trash = dataStrg.splice(id,1);
+                    console.log(`product eliminated`);
+                }
             }
+        }
+        catch(err){
+            console.log(`Ups, file missed ... Error: ${err}`);
         }
 
     }
@@ -61,58 +67,61 @@ class Container {
     }
 }
 
-console.log(`Hi fellows ! Welcome to the ex. 2 ...`);
-let on_Off = prompt(`Let's continue? ... Y / N`);
-while (on_Off === "y" || on_Off === "Y"){
-    let fileName = prompt(` Add up the file name: `);
-    let foo = new Container(fileName);
-    let optionSelected;
-    do{
-        optionSelected = prompt(`Select your tool (1-5): \n
-        1- Save Data,
-        2- Get file by ID,
-        3- Get all files,
-        4- Delete file by ID,
-        5- Delete all.     *** WARNING *** 
-        `)
-        switch(optionSelected){
-            case '1':
-                let stock = [{
-                    name: 'Pure Drive',
-                    brand: 'Babolat',
-                    id: 1
-                },
-                {
-                    name: 'Speed',
-                    brand: 'Head',
-                    id: 2
-                },
-                {
-                    name: 'Pro Staff RF97',
-                    brand: 'Wilson',
-                    id: 3
-                }
-                ]
-                foo.saveData(stock);
-                break;
-            case '2':
-                foo.getById(1);
+// console.log(`Hi fellows ! Welcome to the ex. 2 ...`);
+// let on_Off = prompt(`Let's continue? ... Y / N`);
+// while (on_Off === "y" || on_Off === "Y"){
+//     let fileName = prompt(` Add up the file name: `);
+//     let foo = new Container(fileName);
+//     let optionSelected;
+//     do{
+//         optionSelected = prompt(`Select your tool (1-5): \n
+//         1- Save Data,
+//         2- Get file by ID,
+//         3- Get all files,
+//         4- Delete file by ID,
+//         5- Delete all.     *** WARNING *** 
+//         `)
+//         switch(optionSelected){
+//             case '1':
+//                 let stock = [{
+//                     name: 'Pure Drive',
+//                     brand: 'Babolat',
+//                     id: 1
+//                 },
+//                 {
+//                     name: 'Speed',
+//                     brand: 'Head',
+//                     id: 2
+//                 },
+//                 {
+//                     name: 'Pro Staff RF97',
+//                     brand: 'Wilson',
+//                     id: 3
+//                 }
+//                 ]
+//                 foo.saveData(stock);
+//                 break;
+//             case '2':
+//                 foo.getById(1);
 
-                break;
-            case '3':
-                foo.getAll();
-                break;
-            case '4':
-                foo.deleteById(3);
-                break;
-            case '5':
-                foo.deleteAll();
-                break;
-            default:
-                console.error(`Come one, seriously? You can do it better, let's try one more shot ! `);
-        }
-    }
-    while (typeof optionSelected !== 'number' );
-    on_Off = prompt(`Let's continue? ... Y / N`);
-} 
-console.log(`See you later budy !`);
+//                 break;
+//             case '3':
+//                 foo.getAll();
+//                 break;
+//             case '4':
+//                 foo.deleteById(3);
+//                 break;
+//             case '5':
+//                 foo.deleteAll();
+//                 break;
+//             default:
+//                 console.error(`Come one, seriously? You can do it better, let's try one more shot ! `);
+//         }
+//     }
+//     while (typeof optionSelected !== 'number' );
+//     on_Off = prompt(`Let's continue? ... Y / N`);
+// } 
+// console.log(`See you later budy !`);
+
+// export {Container};
+module.exports = Container;
---
+++ ex3-challenge3.js
@@ -0,0 +1,51 @@
+/* Imports */
+const express = require('express');
+const handler = require('./ex2_fs');
+// import {Container} from 'ex2_fs.js'; ** Type Module Model
+
+/* Controllers */
+const app = express();
+const PORT = 8080;
+let foo = new handler('./products.txt');
+
+/* Server messages */
+const server = app.listen(PORT, () => {
+    console.log(`Server issue by Express.js ready, listen on port ${server.address().port}.`);
+});
+
+/* Error handler */
+server.on('error', error => console.log(`Error: ${error}`));
+
+let cover = {
+    institute: 'Coderhouse.',
+    module: 'Backend.',
+    professor: 'De Ritis, Kevin.',
+    tutor: 'Gil Amoedo, Tomás.',
+    student: 'Romero, Lucas José Gabriel.',
+    mainStyle: 'width: 550px; border: 1px solid greenyellow; border-radius:20px; margin-left: 450px; margin-top: 100px',
+    style: 'color:darkgreen; font-size:25px; padding: 5px'
+}
+
+/* Petitions Handlers */
+app.get('/', (req, res) => {
+    res.send(`
+            <div style="${cover.mainStyle}">
+                <h1 style="${cover.style}; text-align: center">Challenge 3</h1>
+
+                <p style="${cover.style}">Institute: ${cover.institute}</p>
+                <p style="${cover.style}">Module: ${cover.module}</p>
+                <p style="${cover.style}">Professor: ${cover.professor}</p>
+                <p style="${cover.style}">Tutor: ${cover.tutor}</p>
+                <p style="${cover.style}">Student: ${cover.student}</p>
+            </div>
+        `);
+});
+app.get('/products', (req, res) => {
+    foo.getAll().then( (data) => {
+        console.log(data);
+        res.send(data);
+    })
+});
+app.get('/randomProduct', (req, res) => {
+    res.send();
+});
